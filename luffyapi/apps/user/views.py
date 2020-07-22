c27331d3ba57af3156cd9710e464c41859169762
yang <1766829423@qq.com>
2020-07-22 12:10:19 +0800

    格式化了一些代码. 修改了user应用下的action方法

--- luffyapi/apps/user/views.py
+++ luffyapi/apps/user/views.py
@@ -6,39 +6,33 @@ from . import serializer
 from luffyapi.utils.response import APIResponse
 from rest_framework.decorators import action
 from . import models
+
+
 class LoginView(ViewSet):
-    @action(methods=['POST'],detail=False)
-    def login(self,request,*args,**kwargs):
-        ser=serializer.UserSerilaizer(data=request.data)
+    @action(methods=('post',), detail=False)
+    def login(self, request, *args, **kwargs):
+        ser = serializer.UserSerilaizer(data=request.data)
         if ser.is_valid():
-            token=ser.context['token']
-            #ser.context['user'] 是user对象
-            username=ser.context['user'].username
+            token = ser.context['token']
+            # ser.context['user'] 是user对象
+            username = ser.context['user'].username
             # {'code':1
             #  msg:'chengg'
             #  token:'sdfasdf'
             #  username:'root'
             #  }
-            return APIResponse(token=token,username=username)
+            return APIResponse(token=token, username=username)
         else:
-            return APIResponse(code=0,msg=ser.errors)
+            return APIResponse(code=0, msg=ser.errors)
 
-    @action(methods=['GET'], detail=False)
-    def check_telephone(self,request,*args,**kwargs):
+    @action(detail=False)
+    def check_telephone(self, request, *args, **kwargs):
         import re
-        telephone=request.query_params.get('telephone')
-        if not re.match('^1[3-9][0-9]{9}',telephone):
-            return APIResponse(code=0,msg='手机号不合法')
+        telephone = request.query_params.get('telephone')
+        if not re.match('^1[3-9][0-9]{9}', telephone):
+            return APIResponse(code=0, msg='手机号不合法')
         try:
             models.User.objects.get(telephone=telephone)
             return APIResponse(code=1)
         except:
-            return APIResponse(code=0,msg='手机号不存在')
-
-
-
-
-
-
-
-
+            return APIResponse(code=0, msg='手机号不存在')
--- luffyapi/urls.py
+++ luffyapi/urls.py
@@ -1,14 +1,15 @@
-
 from django.contrib import admin
-from django.urls import path,re_path,include
+from django.urls import path, re_path, include
 from django.views.static import serve
 from django.conf import settings
 
 # xadmin的依赖
 import xadmin
+
 xadmin.autodiscover()
 # xversion模块自动注册需要版本控制的 Model
 from xadmin.plugins import xversion
+
 xversion.register_models()
 
 urlpatterns = [
@@ -17,6 +18,6 @@ urlpatterns = [
     path('user/', include('user.urls')),
 
     # media文件夹路径打开了
-    re_path('media/(?P<path>.*)', serve,{'document_root':settings.MEDIA_ROOT}),
+    re_path('media/(?P<path>.*)', serve, {'document_root': settings.MEDIA_ROOT}),
 
 ]
